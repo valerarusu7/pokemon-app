840d9b804582607677aca6b147c3c91a90bd3278
valeriu rusu <valeriu.rusu111@gmail.com>
2023-04-07 12:29:14 +0200

    Adjust weight and height

--- src/components/active-pokemon.tsx
+++ src/components/active-pokemon.tsx
@@ -145,7 +145,7 @@ const ActivePokemon = () => {
               </div>
               <div className="flex grow items-center justify-center gap-2 rounded-full  bg-slate-100 px-4 py-2 font-bold capitalize">
                 <div className="flex items-center justify-center">
-                  {activePokemon.height}m
+                  {activePokemon.height / 10}m
                 </div>
               </div>
             </div>
@@ -155,7 +155,7 @@ const ActivePokemon = () => {
               </div>
               <div className="flex grow items-center justify-center gap-2 rounded-full  bg-slate-100 px-4 py-2 font-bold capitalize">
                 <div className="flex items-center justify-center">
-                  {activePokemon.weight}kg
+                  {activePokemon.weight / 10}kg
                 </div>
               </div>
             </div>
