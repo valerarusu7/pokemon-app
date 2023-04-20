5e5eb824e6d53cad74f9badb274cc1ba61382d48
Valeriu Rusu <valeriu.rusu@lego.com>
2023-04-20 11:35:43 +0200

    Cache

--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -9,23 +9,20 @@ import {
 export const pokemonApi = createApi({
   reducerPath: "pokemonApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
+  keepUnusedDataFor: 1200,
   endpoints: (builder) => ({
     getPokemonByName: builder.query<Pokemon, string>({
       query: (name) => `pokemon/${name}`,
-      keepUnusedDataFor: 1200,
     }),
     getPokemons: builder.query<Pokemons, number>({
       query: (page = 1) =>
         `/pokemon?offset=${page === 1 ? 0 : (page - 1) * 15}&limit=15`,
-      keepUnusedDataFor: 1200,
     }),
     getPokemonSpeciesByName: builder.query<SpeciesResponse, string>({
       query: (name) => `/pokemon-species/${name}`,
-      keepUnusedDataFor: 1200,
     }),
     getEvolutionChainById: builder.query<EvolutionResponse, string>({
       query: (id) => `/evolution-chain/${id}`,
-      keepUnusedDataFor: 1200,
     }),
   }),
 });
